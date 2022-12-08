e6869c4f5bdee867b0c37c778863c1b6cf543f72
李国升 <guosheng.li@pactera.com>
2022-12-08 10:54:55 +0900

    update

---
+++ src/main/java/com/example/entity/Book.java
@@ -0,0 +1,28 @@
+package com.example.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Random;
+import java.util.UUID;
+
+import static com.sun.xml.internal.ws.util.JAXWSUtils.getUUID;
+
+/**
+ * @Description
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Book{
+    private int id;
+    private String name;
+    private String author;
+
+    public Book(String name,String author){
+        this.name = name;
+        this.author = author;
+    }
+
+}
